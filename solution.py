7149c32fc538d8d1c85eb4aa31368e4f8584dd10
letun1987 <123383362+letun1987@users.noreply.github.com>
2023-04-13 12:16:14 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels.stats.proportion import proportions_ztest
+from scipy import stats
+import math
 
 chat_id = 436734951 # Ваш chat ID, не меняйте название переменной
 
